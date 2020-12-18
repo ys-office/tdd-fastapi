165b8d49dd13455824554740b204d3b1c86c994e
Yusuke Sato <yusuke.sato@ys-office.me>
2020-12-18 11:53:01 +0900

    add a lru_cache()

--- fastapi-tdd-docker/project/app/config.py
+++ fastapi-tdd-docker/project/app/config.py
@@ -3,6 +3,7 @@
 
 import logging
 import os
+from functools import lru_cache
 
 from pydantic import BaseSettings
 
@@ -14,7 +15,7 @@ class Settings(BaseSettings):
     environment: str = os.getenv("ENVIRONMENT", "dev")
     testing: bool = os.getenv("TESTING", 0)
 
-
+@lru_cache()
 def get_settings() -> BaseSettings:
     log.info("Loading config settings from the environment...")
     return Settings()
