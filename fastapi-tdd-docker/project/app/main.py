c6e03581deed149e125012dad4bdb7a4e691d70a
Yusuke Sato <yusuke.sato@ys-office.me>
2021-01-02 10:30:18 +0900

    Complate POST Route in Part 1, Chapter 8 - RESTful Routes

--- fastapi-tdd-docker/project/app/main.py
+++ fastapi-tdd-docker/project/app/main.py
@@ -5,7 +5,7 @@ import logging
 
 from fastapi import FastAPI
 
-from app.api import ping
+from app.api import ping, summaries  # updated
 from app.db import init_db
 
 
@@ -15,6 +15,7 @@ log = logging.getLogger("uvicorn")
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(ping.router)
+    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])  # new
 
     return application
 
