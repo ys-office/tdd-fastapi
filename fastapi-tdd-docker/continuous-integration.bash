9c7faf2a025922ef8f0ebb13a8d726cb607a20d5
Yusuke Sato <yusuke.sato@ys-office.me>
2021-02-08 15:19:43 +0900

    move files

--- fastapi-tdd-docker/continuous-integration.bash
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-docker build -f project/Dockerfile.prod -t docker.pkg.github.com/ys-office/fastapi-tdd-docker/summarizer:latest ./project
-docker login docker.pkg.github.com -u ys-office -p ###TOKEN###
-docker push docker.pkg.github.com/ys-office/fastapi-tdd-docker/summarizer:latest
