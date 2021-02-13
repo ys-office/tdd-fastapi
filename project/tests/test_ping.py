108542251125e10a36bc1021d1674380ddbf73e3
Yusuke Sato <yusuke.sato@ys-office.me>
2021-02-13 21:14:29 +0900

    To test, make a quick change to the /ping GET route by removing the exclamation point

--- project/tests/test_ping.py
+++ project/tests/test_ping.py
@@ -1,10 +1,7 @@
 # project/tests/test_ping.py
 
 
-# from app import main
-
-
 def test_ping(test_app):
     response = test_app.get("/ping")
     assert response.status_code == 200
-    assert response.json() == {"environment": "dev", "ping": "pong!", "testing": True}
+    assert response.json() == {"environment": "dev", "ping": "pong", "testing": True}
